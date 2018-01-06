f85b003f36938041e66e81ab0b04541b633ec77d
Reece Orbuck <reeceorbuck@me.com>
2018-01-06 16:46:38 +0800

    feature images, toc, thumbnails

--- _site/scripts/post.js
+++ _site/scripts/post.js
@@ -13,3 +13,32 @@ $.bigfoot({
         "</a>"
     )
   });
+
+  
+  function changeSourceAll() {
+    
+    var images = document.getElementById("postMain").querySelectorAll('img[alt*="thumbnail"]')
+    var imagesLength = images.length
+    for (var i = 0; i < imagesLength; i++) {
+        var oldImage = images[i];
+        var newAnchor = document.createElement("a");
+        newAnchor.href = oldImage.src.replace("/thumbnails", "")
+        newAnchor.className = "carousel_item";
+
+        var newImage = document.createElement("img");
+        newImage.className = "carousel_image2 profilePic";
+        
+        // var lastPath = oldImage.src.lastIndexOf("/")
+        // newImage.src = oldImage.src.slice(0, lastPath) + "/thumbnails" + oldImage.src.slice(lastPath);
+        newImage.src = oldImage.src;
+
+        newAnchor.appendChild(newImage);
+
+        oldImage.parentNode.insertBefore(newAnchor,oldImage)
+        oldImage.parentNode.removeChild(oldImage)
+    }
+}
+window.onload = function() {
+    changeSourceAll();
+    baguetteBox.run('.carousel_item');
+};
